023b8eb8debe512a09bc64c5389df5c8c74b71e0
jsueiro <juan.sueiro@outlook.com>
2022-03-24 16:39:26 +0100

    initial basic db functionality as text

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from asyncore import write
+from flask import Flask, render_template, request, redirect
 app = Flask(__name__)
 
 
@@ -10,3 +11,21 @@ def main_page():
 @app.route("/<string:page_name>")
 def html_page(page_name):
     return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', 'a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong. Try again. '
